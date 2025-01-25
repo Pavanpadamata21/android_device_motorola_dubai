bb4752053350064c7ded53445caa0f4c084b8974
PavanPadamata <pavanpadmata2002@gmail.com>
2025-01-26 01:15:52 +0530

    dubai:kernel:Update kernel path.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,7 +62,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
-TARGET_KERNEL_SOURCE := kernel/motorola/dubai
+TARGET_KERNEL_SOURCE := kernel/motorola/sm7325
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig vendor/lineage_moto-lahaina.config
 
 # Kernel modules - WLAN
